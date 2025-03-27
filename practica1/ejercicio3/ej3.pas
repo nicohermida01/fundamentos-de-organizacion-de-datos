9264dcfbc26ed808a79fc9cad16b277444142c5d
Nico Hermida <hermida.nicolas101@gmail.com>
2025-03-27 19:19:45 -0300

    ejercicio 3 corregido

--- practica1/ejercicio3/ej3.pas
+++ practica1/ejercicio3/ej3.pas
@@ -11,84 +11,122 @@ var
   fileOfEmpleados: file of empleado;
   emp: empleado;
   fileName, search: string;
+  opcion: integer;
 begin
-  fileName := 'empleados';
-  assign(fileOfEmpleados, fileName);
-  rewrite(fileOfEmpleados);
+  writeln('Menu de opciones:');
+  writeln('1. Crear archivo de empleados');
+  writeln('0. Salir');
+  write('Ingrese una opcion: ');
+  readln(opcion);
 
-  writeln('Ingrese los datos de un empleado:');
-  write('Apellido: ');
-  readln(emp.apellido);
-  while (emp.apellido <> 'fin') do
+  if (opcion = 1) then
   begin
-    write('Nombre: ');
-    readln(emp.nombre);
-    write('Edad: ');
-    readln(emp.edad);
-    write('DNI: ');
-    readln(emp.dni);
-    write('Numero de empleado: ');
-    readln(emp.numEmp);
+    write('Ingrese el nombre del archivo: ');
+    readln(fileName);
+    assign(fileOfEmpleados, fileName);
+    rewrite(fileOfEmpleados);
 
-    write(fileOfEmpleados, emp);
     writeln('Ingrese los datos de un empleado:');
     write('Apellido: ');
     readln(emp.apellido);
-  end;
-
-  close(fileOfEmpleados);
-
-  // Punto b
-  assign(fileOfEmpleados, fileName);
-  reset(fileOfEmpleados);
-
-  // punto b1
-  write('Ingrese un nombre o apellido para buscar: ');
-  readln(search);
-
-  while not eof(fileOfEmpleados) do
-  begin
-    read(fileOfEmpleados, emp);
-
-    if (emp.apellido = search) or (emp.nombre = search) then
+    while (emp.apellido <> 'fin') do
     begin
-      writeln('Apellido: ', emp.apellido);
-      writeln('Nombre: ', emp.nombre);
-      writeln('Edad: ', emp.edad);
-      writeln('DNI: ', emp.dni);
-      writeln('Numero de empleado: ', emp.numEmp);
-      writeln('--------------------------------');
+      write('Nombre: ');
+      readln(emp.nombre);
+      write('Edad: ');
+      readln(emp.edad);
+      write('DNI: ');
+      readln(emp.dni);
+      write('Numero de empleado: ');
+      readln(emp.numEmp);
+
+      write(fileOfEmpleados, emp);
+      writeln('Ingrese los datos de un empleado:');
+      write('Apellido: ');
+      readln(emp.apellido);
     end;
-  end;
 
-  // Punto b2
-  seek(fileOfEmpleados, 0);
-  writeln('Contenido del archivo:');
-  while not eof(fileOfEmpleados) do
-  begin
-    read(fileOfEmpleados, emp);
-    writeln('Apellido: ', emp.apellido,
-            ', Nombre: ', emp.nombre,
-            ', Edad: ', emp.edad,
-            ', DNI: ', emp.dni,
-            ', Numero de empleado: ', emp.numEmp);
-  end;
+    close(fileOfEmpleados);
+    writeln('Archivo creado con exito.');
 
-  // punto b3
-  seek(fileOfEmpleados, 0);
-  writeln('Empleados mayores de 70 años:');
-  while not eof(fileOfEmpleados) do
-  begin
-    read(fileOfEmpleados, emp);
-    if (emp.edad > 70) then
+    writeln('Menu de opciones:');
+    writeln('1. Buscar empleado por nombre o apellido');
+    writeln('2. Mostrar contenido del archivo');
+    writeln('3. Mostrar empleados mayores de 70 años');
+    writeln('0. Salir');
+    write('Ingrese una opcion: ');
+    readln(opcion);
+
+    while (opcion <> 0) do
     begin
-      writeln('Apellido: ', emp.apellido,
-              ', Nombre: ', emp.nombre,
-              ', Edad: ', emp.edad,
-              ', DNI: ', emp.dni,
-              ', Numero de empleado: ', emp.numEmp);
-    end;
-  end;
+      if (opcion = 1) then
+      begin
+        reset(fileOfEmpleados);
+        write('Ingrese un nombre o apellido para buscar: ');
+        readln(search);
 
-  close(fileOfEmpleados);
+        while not eof(fileOfEmpleados) do
+        begin
+          read(fileOfEmpleados, emp);
+
+          if (emp.apellido = search) or (emp.nombre = search) then
+          begin
+            writeln('Apellido: ', emp.apellido);
+            writeln('Nombre: ', emp.nombre);
+            writeln('Edad: ', emp.edad);
+            writeln('DNI: ', emp.dni);
+            writeln('Numero de empleado: ', emp.numEmp);
+            writeln('--------------------------------');
+          end;
+        end;
+
+        close(fileOfEmpleados);
+      end
+      else if (opcion = 2) then
+      begin
+        reset(fileOfEmpleados);
+
+        writeln('Contenido del archivo:');
+        while not eof(fileOfEmpleados) do
+        begin
+          read(fileOfEmpleados, emp);
+          writeln('Apellido: ', emp.apellido,
+                  ', Nombre: ', emp.nombre,
+                  ', Edad: ', emp.edad,
+                  ', DNI: ', emp.dni,
+                  ', Numero de empleado: ', emp.numEmp);
+        end;
+
+        close(fileOfEmpleados);
+      end
+      else if (opcion = 3) then
+      begin
+        reset(fileOfEmpleados);
+
+        writeln('Empleados mayores de 70 años:');
+        while not eof(fileOfEmpleados) do
+        begin
+          read(fileOfEmpleados, emp);
+          if (emp.edad > 70) then
+          begin
+            writeln('Apellido: ', emp.apellido,
+                    ', Nombre: ', emp.nombre,
+                    ', Edad: ', emp.edad,
+                    ', DNI: ', emp.dni,
+                    ', Numero de empleado: ', emp.numEmp);
+          end;
+        end;
+
+        close(fileOfEmpleados);
+      end;
+
+      writeln('Menu de opciones:');
+      writeln('1. Buscar empleado por nombre o apellido');
+      writeln('2. Mostrar contenido del archivo');
+      writeln('3. Mostrar empleados mayores de 70 años');
+      writeln('0. Salir');
+      write('Ingrese una opcion: ');
+      readln(opcion);
+    end
+  end;   
 end.
